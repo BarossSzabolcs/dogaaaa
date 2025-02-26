c75d62c656abf59637764a59ec32ec02ebd12b5e
BarossSzabolcs <150019742+BarossSzabolcs@users.noreply.github.com>
2025-02-26 12:39:26 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class EmailValidator
+    {
+        public bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            { 
+                return false;
+            }
+            int atIndex = email.IndexOf("@");
+            if (atIndex <= 0 || atIndex != email.LastIndexOf("@"))
+            { 
+                return false ;
+            }
+            string localPart = email.Substring(0, atIndex);
+            string domainPart = email.Substring(atIndex + 1);
+            if (string.IsNullOrEmpty(domainPart) || string.IsNullOrEmpty(localPart))
+            { 
+                return false;
+            }
+            int dotIndex = email.IndexOf(".");
+            if (dotIndex <= 0)
+            {
+                return false;
+            }
+            
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string email;
+            ConsoleKey key;
+            do
+            {
+                Console.Write("Add meg az e-mail címet: ");
+                email = (Console.ReadLine());
+
+                var email2 = new EmailValidator();
+
+                Console.WriteLine($"{email2.IsValidEmail(email)}");
+                Console.WriteLine("\nÚj e-mail megadása(ENTER)  |  Kilépés(ESC)\n");
+                key = Console.ReadKey(true).Key;
+
+            } while (key != ConsoleKey.Escape);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class EmailValidatorTests
+    {
+        [TestMethod]
+        public void IsValidEmail_ReturnTrue_ForValidEmail()
+        {
+            var validator = new EmailValidator();
+            string input = "test@example.com";
+
+            bool result = validator.IsValidEmail(input);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void EmptyEmail_ReturnsFalse()
+        {
+            var validator = new EmailValidator();
+            string input = "";
+
+            bool result = validator.IsValidEmail(input);
+
+            Assert.IsFalse(result);
+        }
+        
+        [TestMethod]
+        public void EmailWithoutAtSymbol_ReturnsFalse()
+        {
+            var validator = new EmailValidator();
+            string input = "testexample.com";
+
+            bool result = validator.IsValidEmail(input);
+
+            Assert.IsFalse(result);
+        }
+        
+        [TestMethod]
+        public void EmailWithMultipleAtSymbols_ReturnsFalse()
+        {
+            var validator = new EmailValidator();
+            string input = "test@@example.com";
+
+            bool result = validator.IsValidEmail(input);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void EmailWithoutDomain_ReturnsFalse()
+        {
+            var validator = new EmailValidator();
+            string input = "test@";
+
+            bool result = validator.IsValidEmail(input);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void EmailWithoutLocalPart_ReturnsFalse()
+        {
+            var validator = new EmailValidator();
+            string input = "@example.com";
+
+            bool result = validator.IsValidEmail(input);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void EmailWithoutTLD_ReturnsFalse()
+        {
+            var validator = new EmailValidator();
+            string input = "test@example";
+
+            bool result = validator.IsValidEmail(input);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
